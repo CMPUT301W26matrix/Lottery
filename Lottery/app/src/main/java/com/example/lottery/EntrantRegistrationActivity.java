0e64d3df4435f26b8064d18f342d790d5f4b7cdf
zhA_zam <zphazam432@gmail.com>
2026-03-09 04:16:30 -0600

    feat: set up role selection screen

---
+++ Lottery/app/src/main/java/com/example/lottery/EntrantRegistrationActivity.java
@@ -0,0 +1,23 @@
+package com.example.lottery;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class EntrantRegistrationActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_entrant_registration);
+
+        // Initialize firebase
+        db = FirebaseFirestore.getInstance();
+    }
+}
--- Lottery/app/src/main/java/com/example/lottery/MainActivity.java
+++ Lottery/app/src/main/java/com/example/lottery/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lottery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +15,66 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Button signInButton;
     private Button entrantButton;
     private Button organizerButton;
     private Button adminButton;
+    private TextView chooseRoleText;
+    private TextView signInPrompt;
 
     private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Initialize firebase
         db = FirebaseFirestore.getInstance();
 
-        signInButton = findViewById(R.id.btnSignIn);
+        // Role Selection buttons
         entrantButton = findViewById(R.id.entrant_login_button);
         organizerButton = findViewById(R.id.organizer_login_button);
         adminButton = findViewById(R.id.admin_login_button);
+
+        // Sign-in button (for registered users)
+        signInButton = findViewById(R.id.btnSignIn);
+
+        // Text views
+        chooseRoleText = findViewById(R.id.tvChooseRole);
+        signInPrompt = findViewById(R.id.tvSignInHint);
+
+        entrantButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EntrantRegistrationActivity.class);
+            startActivity(intent);
+        });
+
+        organizerButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, OrganizerRegistrationActivity.class);
+            startActivity(intent);
+        });
+
+        adminButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AdminSignInActivity.class);
+            startActivity(intent);
+        });
+
+        signInButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, GeneralSignInActivity.class);
+            startActivity(intent);
+        });
+
     }
+
 }
