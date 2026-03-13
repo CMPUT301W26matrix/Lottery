4c00e506b632648423d61f5ca752209b342d0371
Youhao Zhao <128596662+youhaozhao@users.noreply.github.com>
2026-03-13 13:31:11 -0600

    chore: remove unused Organizer class and dead color/string resources

--- Lottery/app/src/main/java/com/example/lottery/Organizer.java
+++
@@ -1,7 +0,0 @@
-package com.example.lottery;
-
-public class Organizer extends User{
-    public Organizer(String device_info, String password, String phone_number, String userId, String username) {
-        super(device_info, password, phone_number, userId, "organizer", username);
-    }
-}
